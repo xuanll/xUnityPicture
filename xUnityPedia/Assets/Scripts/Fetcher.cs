e19b573d55a7973ef34cf7042a6a801c10a685ab
Antonis <linxuan.lv@kunlun-inc.com>
2017-03-07 20:33:40 +0800

    init project - finish load picture from web using WWW

---
+++ xUnityPedia/Assets/Scripts/Fetcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fetcher : MonoBehaviour {
+
+	private IEnumerator coroutine;
+
+	public void LoadPictureToImage(string url, Image image){
+		coroutine = GetTextureFromURL (url, image);
+
+		StartCoroutine(coroutine);
+	}
+		
+
+	private IEnumerator GetTextureFromURL(string url, Image image){
+		WWW www = new WWW(url);
+		yield return www;
+
+		if(www != null && string.IsNullOrEmpty(www.error))
+		{
+			Texture2D texture=www.texture;
+
+			Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+
+			RectTransform rt = image.transform.GetComponent<RectTransform> ();
+			rt.sizeDelta = new Vector2 (texture.width, texture.height);
+
+			image.sprite = sprite;
+		}
+	}
+}
---
+++ xUnityPedia/Assets/Scripts/MainManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class MainManager : MonoBehaviour {
+
+	[SerializeField]
+	private Image image; 
+
+	void OnEnable(){
+		string smatisan_logo = "http://t.cn/Ri0b37C"; 		//shorten by http://dwz.wailian.work/
+		string smatisan_cloud = "http://t.cn/Ri0GhGZ";
+
+		// Fetcher 
+		if (image != null) {
+			if (image.transform.GetComponent<Fetcher> () != null)
+				image.transform.GetComponent<Fetcher> ().LoadPictureToImage (smatisan_cloud, image);
+			else
+				image.gameObject.AddComponent<Fetcher> ().LoadPictureToImage (smatisan_logo, image);
+		} else {
+			Debug.Log ("Image is null, please check..");
+		}
+		
+	}
+}

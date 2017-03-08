c0da7e9cb01ee8e2b885ad71b74b5bac16e52144
Antonis <linxuan.lv@kunlun-inc.com>
2017-03-08 14:52:25 +0800

    add new Scene and add load image logic in empty Canvas

--- xUnityPedia/Assets/Scripts/MainManager.cs
+++ xUnityPedia/Assets/Scripts/MainManager.cs
@@ -13,6 +13,8 @@ public class MainManager : MonoBehaviour {
 		string smatisan_logo = "http://t.cn/Ri0b37C"; 		//shorten by http://dwz.wailian.work/
 		string smatisan_cloud = "http://t.cn/Ri0GhGZ";
 
+		string yougForYou_logo = "http://t.cn/RiW2K6q"; 
+
 		// Fetcher 
 		if (image != null) {
 			if (image.transform.GetComponent<Fetcher> () != null)
@@ -21,7 +23,23 @@ public class MainManager : MonoBehaviour {
 				image.gameObject.AddComponent<Fetcher> ().LoadPictureToImage (smatisan_logo, image);
 		} else {
 			Debug.Log ("Image is null, please check..");
+
+			GameObject go = new GameObject ("image");
+
+			// set Parent
+			go.transform.SetParent (transform);
+
+			// add Image component
+			go.AddComponent<Image> ();
+
+			// set the position
+			go.transform.localPosition = Vector3.zero;
+
+			//set image to newly created Image
+			image = go.GetComponent<Image>();
+
+			// add Fetcher and load picture
+			go.AddComponent<Fetcher>().LoadPictureToImage (yougForYou_logo, image);
 		}
-		
 	}
 }
